2e47ea2527b26f4a574a6cd4d445d89df9bd1b7f
RealPenguin <mrjanggo858@gmail.com>
2021-09-28 23:13:05 +0700

    delete auth stuff

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -2,13 +2,6 @@ import { Router } from "express";
 import { Login, Register } from "../../db/services/auth.service";
 const router = Router();
 
-router.get('/', (req, res) => {
-    if (req.session.user) {
-        res.send({ loggedIn: true, user: req.session.user });
-    } else {
-        res.send({ loggedIn: false });
-    }
-})
 
 router.post('/login', Login)
 router.post('/register', Register)
