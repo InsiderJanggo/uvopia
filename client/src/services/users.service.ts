efce514e5bb71f0f475782734cc7c01402fcbb05
RealPenguin <mrjanggo858@gmail.com>
2021-09-28 15:51:31 +0700

    updated client user service

--- client/src/services/users.service.ts
+++ client/src/services/users.service.ts
@@ -1,6 +1,9 @@
 import httpCommon from "../http-common";
 
 class UserService {
+    getAll() {
+        return httpCommon.get('/users')
+    }
     getOne(id: any) {
         return httpCommon.get(`/users/${id}`)
     }
