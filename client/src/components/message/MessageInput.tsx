43bf388187cd62d745c5fe35a7ca4a214525d950
RealPenguin <mrjanggo858@gmail.com>
2021-09-29 08:33:14 +0700

    updated message input and homepage

--- client/src/components/message/MessageInput.tsx
+++ client/src/components/message/MessageInput.tsx
@@ -1,7 +1,37 @@
-
+import {
+    Button,
+    InputGroup,
+    FormControl,
+    Form
+} from 'react-bootstrap'
+import useForm from '../../hooks/useForm'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const MessageInput = () => {
+    const [input, setInput] = useForm({
+        message: ''
+    })
 
+    return(
+        <Form autoComplete="off" autoCorrect="off">
+        <InputGroup className="mb-3">
+          <FormControl
+                placeholder="Enter Your Message"
+                aria-label="Message Input"
+                name="message"
+                value={input.message}
+                onChange={setInput}
+                aria-describedby="basic-addon2"
+                required
+            />
+            <Button variant="outline-primary" id="button-addon2" type="submit">
+                 <FontAwesomeIcon icon={ faAngleRight } />
+            </Button>
+          </InputGroup>
+      </Form>
+    )
 }
 
 export default MessageInput
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,4 @@
-import React from "react";
 import Header from "../components/Header";
-
-export interface User {
-    username: string;
-    email?: string;
-    password: string
-}
 
 const Home = () => {
     return (
